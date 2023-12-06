fa9455cba47953f6015b40b95016630efefe8f8e
universe-sync <universe.sync55@gmail.com>
2023-12-06 21:51:01 +0900

    1206

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,30 @@ window.addEventListener('scroll', () => {
         button.classList.remove('button-up');
     }
 });
+
+//ギャラリーのスライダー
+$('.p-plan__slider').slick({
+  autoplay: true, //自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 5000, // 自動再生のスピードを10秒に設定
+  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+  speed: 1000, //スライドのスピード。初期値は300。
+  slidesToShow: 3, //スライドを画面に3枚見せる
+  slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
+  centerMode: true, //要素を中央ぞろえにする
+  variableWidth: true, //幅の違う画像の高さを揃えて表示
+  dots: true, //下部ドットナビゲーションの表示
+  responsive: [
+    {
+      breakpoint: 768, // 768px以上の画面サイズ
+      settings: {
+        slidesToShow: 5, // 5枚の画像を表示
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    }
+    // 他のブレイクポイント設定もここに追加可能
+  ]
+});
